0019705baab2c324ff189b2d445fde604435a630
Balazs Szalai <szalai.balazs.david@gmail.com>
2020-07-16 19:51:41 -0400

    Add endpoint functionality
    Implemented GET and PATCH User functionality

--- backend/app/main/service.py
+++ backend/app/main/service.py
@@ -4,12 +4,29 @@ from app.main.util import get_response
 from flask import abort, jsonify
 
 
+def user_to_short_string(user):
+    tests = []
+    for result in User.results:
+        if result.test_id not in tests:
+            # todo: add test name in addition to test id
+            tests.append(result.test_id)
+    return {
+        'id': user.id,
+        'name': user.name if user.name is not None else user.auth0_id,
+        'tests': tests
+    }
+
+
 def get_user_details(user_id):
-    abort(404)
+    user = User.query.get(user_id)
+    return get_response(user_to_short_string(user))
 
 
-def update_user(id, name):
-    abort(404)
+def update_user(user_id, name):
+    user = User.query.get(user_id)
+    user.name = name
+    db.session.commit()
+    return get_user_details(user_to_short_string(user))
 
 
 def create_test(name):
@@ -32,5 +49,5 @@ def update_test_result(user_id, result_id, time, value):
     abort(404)
 
 
-def delete_result(user_id, id):
+def delete_result(user_id, result_id):
     abort(404)
