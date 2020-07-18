d17c7a5126eb750abaf40bae18042e1f1f86f5dc
Balazs Szalai <szalai.balazs.david@gmail.com>
2020-07-18 09:33:37 -0400

    Add new front end from example
    Used this example to create a starter front end:
    https://www.kirupa.com/react/creating_single_page_app_react_using_react_router.htm

---
+++ frontend/src/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <p>The purpose of this application is to enable people to collect and visualize their medical test results over long periods of time, thus get a better understanding about how their health is changing.</p>
+
+        <p>Smart test devices could allow seemless integration with our application by automatically uploading test results. (With prior consent from the user, of course.)</p>
+      </div>
+    );
+  }
+}
+
+export default Home;
