54c06eee47fee1b42d0945eeb34a0b08b93f0358
Balazs Szalai <szalai.balazs.david@gmail.com>
2020-07-18 10:48:39 -0400

    Add a basic login logout
    Followed this tutorial:
    https://auth0.com/docs/quickstart/spa/react/01-login

--- frontend/src/Main.js
+++ frontend/src/Main.js
@@ -7,6 +7,9 @@ import {
 import Home from "./Home";
 import Stuff from "./Stuff";
 import Contact from "./Contact";
+import LoginButton from "./Login";
+import LogoutButton from "./Logout";
+import Profile from "./Profile";
 
 class Main extends Component {
     render() {
@@ -18,11 +21,17 @@ class Main extends Component {
                         <li><NavLink exact to="/">Home</NavLink></li>
                         <li><NavLink to="/stuff">Stuff</NavLink></li>
                         <li><NavLink to="/contact">Contact</NavLink></li>
+                        <li><NavLink to="/profile">Profile</NavLink></li>
+                        <li><NavLink to="/login">Login</NavLink></li>
+                        <li><NavLink to="/logout">Logout</NavLink></li>
                     </ul>
                     <div className="content">
                         <Route exact path="/" component={Home}/>
                         <Route path="/stuff" component={Stuff}/>
                         <Route path="/contact" component={Contact}/>
+                        <Route path="/profile" component={Profile}/>
+                        <Route path="/login" component={LoginButton}/>
+                        <Route path="/logout" component={LogoutButton}/>
                     </div>
                 </div>
             </HashRouter>
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,8 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Main from "./Main";
 import "./index.css";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 ReactDOM.render(
-  <Main/>,
+  <Auth0Provider
+    domain="balazsszalai.auth0.com"
+    clientId="Ijleo40NJaKF4t0IUPCfXovwzUgyzGnj"
+    redirectUri="http://127.0.0.1:3000/#/"
+  >
+    <Main/>
+  </Auth0Provider>,
   document.getElementById("root")
 );
