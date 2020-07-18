bb9c980971c9eea206b9d44da403b3e41ddf45b0
Balazs Szalai <szalai.balazs.david@gmail.com>
2020-07-18 11:19:49 -0400

    Add some more data to Profile.js

--- frontend/src/Profile.js
+++ frontend/src/Profile.js
@@ -1,8 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 
 const Profile = () => {
-  const { user, isAuthenticated } = useAuth0();
+  const { user, isAuthenticated, getAccessTokenSilently } = useAuth0();
+  const [userMetadata, setUserMetadata] = useState(null);
+
+  useEffect(() => {
+      const getUserMetadata = async () => {
+        const domain = "balazsszalai.auth0.com";
+
+        try {
+          const accessToken = await getAccessTokenSilently({
+            audience: `https://${domain}/api/v2`,
+            scope: "delete:measurement",
+          });
+
+          const userDetailsByIdUrl = `https://${domain}/api/v2/users/${user.sub}`;
+
+          const metadataResponse = await fetch(userDetailsByIdUrl, {
+            headers: {
+              Authorization: `Bearer ${accessToken}`,
+            },
+          });
+
+          const { user_metadata } = await metadataResponse.json();
+
+          setUserMetadata(user_metadata);
+        } catch (e) {
+          console.log(e.message);
+        }
+      };
+
+      getUserMetadata();
+    }, []);
 
   return (
     isAuthenticated && (
@@ -10,6 +40,12 @@ const Profile = () => {
         <img src={user.picture} alt={user.name} />
         <h2>{user.name}</h2>
         <p>{user.email}</p>
+        <h3>User Metadata</h3>
+        {userMetadata ? (
+          <pre>{JSON.stringify(userMetadata, null, 2)}</pre>
+        ) : (
+          "No user metadata defined"
+        )}
       </div>
     )
   );
