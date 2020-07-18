6c608e6bde9f49f0355ab1b2564229c0b1e7cd3f
Balazs Szalai <szalai.balazs.david@gmail.com>
2020-07-18 12:38:34 -0400

    Add logout callback

--- frontend/src/Logout.js
+++ frontend/src/Logout.js
@@ -4,7 +4,9 @@ import { useAuth0 } from "@auth0/auth0-react";
 const LogoutButton = () => {
   const { logout } = useAuth0();
 
-  return <button onClick={() => logout()}>Log Out</button>;
+  return <button onClick={() => logout({
+      returnTo: "http://127.0.0.1:3000/#/",
+    })}>Log Out</button>;
 };
 
 export default LogoutButton;
