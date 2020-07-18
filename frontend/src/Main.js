fd3aafaa5a4bbb40df304f0ad14f3d9462e57458
Balazs Szalai <szalai.balazs.david@gmail.com>
2020-07-18 16:43:58 -0400

    Remove obsolete files

--- frontend/src/Main.js
+++ frontend/src/Main.js
@@ -7,9 +7,6 @@ import {
 import Home from "./Home";
 import Stuff from "./Stuff";
 import Contact from "./Contact";
-import LoginButton from "./Login";
-import LogoutButton from "./Logout";
-import Profile from "./Profile";
 import App from "./App";
 
 class Main extends Component {
@@ -23,17 +20,11 @@ class Main extends Component {
                         <li><NavLink exact to="/">Home</NavLink></li>
                         <li><NavLink to="/stuff">Stuff</NavLink></li>
                         <li><NavLink to="/contact">Contact</NavLink></li>
-                        <li><NavLink to="/profile">Profile</NavLink></li>
-                        <li><NavLink to="/login">Login</NavLink></li>
-                        <li><NavLink to="/logout">Logout</NavLink></li>
                     </ul>
                     <div className="content">
                         <Route exact path="/" component={Home}/>
                         <Route path="/stuff" component={Stuff}/>
                         <Route path="/contact" component={Contact}/>
-                        <Route path="/profile" component={Profile}/>
-                        <Route path="/login" component={LoginButton}/>
-                        <Route path="/logout" component={LogoutButton}/>
                     </div>
                 </div>
             </HashRouter>
