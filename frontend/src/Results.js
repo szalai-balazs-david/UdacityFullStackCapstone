165c87b7335055aeadef1407387c79f15eea6902
Balazs Szalai <szalai.balazs.david@gmail.com>
2020-07-19 17:59:02 -0400

    Add results page to front end

--- frontend/src/Results.js
+++ frontend/src/Results.js
@@ -1,62 +1,53 @@
 import React, { useEffect, useState } from 'react';
-import { useAuth0, withAuth0 } from '@auth0/auth0-react';
+import { useAuth0 } from '@auth0/auth0-react';
 
-class Result extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      error: null,
-      isLoaded: false,
-      items: []
-    };
-  }
+function Result() {
+  const { getAccessTokenSilently } = useAuth0();
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [items, setItems] = useState([]);
 
-  componentDidMount() {
-    const {getAccessTokenSilently} = this.props.auth0;
-    const token = getAccessTokenSilently();
-    fetch("/results", {
+  useEffect(() => {
+      getAccessTokenSilently({
+        audience: 'https://medical-measurement',
+        scope: 'get:doctors',
+      })
+      .then(token => fetch("/results", {
           headers: {
             Authorization: `Bearer ${token}`,
           },
-        })
-    .then(res => res.json())
-    .then(
-      (result) => {
-        this.setState({
-          isLoaded: true,
-          items: result.message.data
-        });
-      },
-      // Note: it's important to handle errors here
-      // instead of a catch() block so that we don't swallow
-      // exceptions from actual bugs in components.
-      (error) => {
-        this.setState({
-          isLoaded: true,
-          error
-        });
-      }
-    )
-  }
+        }))
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setItems(result.message);
+          setIsLoaded(true);
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      )
+  }, [])
 
-  render() {
-    const { error, isLoaded, items } = this.state;
-    if (error) {
-      return <div>Error: {error.message}</div>;
-    } else if (!isLoaded) {
-      return <div>Loading...</div>;
-    } else {
-      return (
-        <ul>
-          {items.map(item => (
-            <li key={item.id}>
-              ID: {item.test_id} at {item.time}: {item.value}
-            </li>
-          ))}
-        </ul>
-      );
-    }
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  } else if (!isLoaded) {
+    return <div>Loading...</div>;
+  } else {
+    return (
+      <ul>
+        {items.data.map(item => (
+          <li key={item.id}>
+            ID: {item.test_id} at {item.time}: {item.value}
+          </li>
+        ))}
+      </ul>
+    );
   }
 }
 
-export default withAuth0(Result);
+export default Result;
