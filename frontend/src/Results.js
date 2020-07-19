de5290f9aee1ec3153b73dc25350ce6d0b99dd07
Balazs Szalai <szalai.balazs.david@gmail.com>
2020-07-19 19:35:14 -0400

    Add option to front end to POST results

--- frontend/src/Results.js
+++ frontend/src/Results.js
@@ -6,6 +6,7 @@ function Result() {
   const [error, setError] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
   const [items, setItems] = useState([]);
+  const [value, setValue] = useState(0);
 
   useEffect(() => {
       getAccessTokenSilently({
@@ -33,6 +34,37 @@ function Result() {
       )
   }, [])
 
+  const handleChange = (evt) => {
+      setValue(evt.target.value);
+  }
+
+  const handleSubmit = (evt) => {
+      getAccessTokenSilently({
+        audience: 'https://medical-measurement',
+        scope: 'get:doctors',
+      })
+      .then(token => fetch("/results", {
+          method: 'POST',
+          headers: {
+            Authorization: `Bearer ${token}`,
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({test_id: 1, time: new Date().toLocaleString(), value: value})
+        }))
+      .then(res => res.json())
+      .then(
+        (result) => {
+          alert(`Submitting Result ${value}`);
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          alert('Error: ' + error);
+        }
+      )
+  }
+
   if (error) {
     return <div>Error: {error.message}</div>;
   } else if (!isLoaded) {
@@ -42,9 +74,15 @@ function Result() {
       <ul>
         {items.data.map(item => (
           <li key={item.id}>
-            ID: {item.test_id} at {item.time}: {item.value}
+            Test ID: {item.test_id} at {item.time}: {item.value}
           </li>
         ))}
+      <form onSubmit={handleSubmit}>
+        <label>
+          Value:
+          <input type="number" value={value.value} onChange={handleChange} /></label>
+        <input type="submit" value="Submit" />
+      </form>
       </ul>
     );
   }
